de9b8ba611c76b31662f784a99db6f7c4c1698ea
钟致远电脑服务 <58477698+zzysite@users.noreply.github.com>
2020-05-01 11:37:14 +0800

    已经写好JavaScript的代码了

---
+++ script.js
@@ -0,0 +1,19 @@
+//获取按钮对象
+var btn = document.getElementById('btn');
+//创建1-1000以内的随机数
+var number = Math.floor(Math.random()*1000+1);
+
+//创建按钮事件
+btn.onclick = function () {
+  //获取文本框的值
+  var text = document.getElementById('text').value;
+  if (text == number) {
+    alert("猜对了！");
+  }else if (text > number) {
+    alert("猜大了");
+  }else if (text < number) {
+    alert("猜小了");
+  }else {
+    alert("请输入1-1000以内的数字！")
+  }
+}
